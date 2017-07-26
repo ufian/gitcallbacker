2a2a463a56ff3c39da7022333ff291b5748acab8
ufian <ufian@ya.ru>
2017-07-25 18:17:26 -0700

    Add callback method

--- service.py
+++ service.py
@@ -2,10 +2,32 @@
 
 __author__ = 'ufian'
 
+import contextlib
+import os
 import sh
 from flask import Flask
 app = Flask(__name__)
 
+
+
+@contextlib.contextmanager
+def cd(path):
+   old_path = os.getcwd()
+   os.chdir(path)
+   try:
+       yield
+   finally:
+       os.chdir(old_path)
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/restart')
+def hello_world():
+    with cd('/root/twitory'):
+        sh.git("pull")
+        sh.docker_compose("restart")
+
+    with cd('/root/nginx'):
+        sh.docker_compose("restart")
