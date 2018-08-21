f6adbb427206c32fb51dfa5a08b83c9d4623f331
ufian <ufian@ya.ru>
2018-08-21 09:19:08 -0700

    Add notify method

--- service.py
+++ service.py
@@ -6,9 +6,40 @@ import contextlib
 import os
 import sh
 import threading
-from flask import Flask, abort
+import yaml
+import requests
+from flask import Flask, abort, request
+
+
 app = Flask(__name__)
 
+config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'notify.yaml')
+try:
+    config = {}
+    with open(config_path) as f:
+        config = yaml.load(f.read())
+except:
+    pass
+
+def send_message(configchat, text):
+    api_url = 'https://api.telegram.org/bot{}'.format(configchat['token'])
+    name = 'sendMessage'
+    
+    params = {
+        'text': text,
+        'chat_id': configchat['user']
+    }
+    
+    res = requests.request(
+        'POST',
+        api_url + '/{0}'.format(name),
+        params=params,
+        timeout=30
+    )
+
+def send(channel, message):
+    if channel in config:
+        send_message(config[channel], message)
 
 
 @contextlib.contextmanager
@@ -23,6 +54,16 @@ def cd(path):
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/notify', methods=['GET', 'POST'])
+def notify():
+    channel = request.args.get('channel')
+    text = request.args.get('text')
+    
+    if channel and text:
+        send(channel, text)
+
+    return "Ok"
 
 @app.route('/deploy/<service>', methods=['GET', 'POST'])
 def deploy(service):
